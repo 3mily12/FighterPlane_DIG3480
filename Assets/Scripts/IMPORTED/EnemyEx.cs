ec671dd07b37c6f9617bdb3d8020f25fde7e5f0d
3mily12 <ecmartin2086@gmail.com>
2025-11-13 01:52:30 -0500

    Added game import for assets and individual scenes to avoid conflicts.

---
+++ Assets/Scripts/IMPORTED/EnemyEx.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyEx : MonoBehaviour
+{
+
+    public GameObject explosionPrefab;
+    
+    private GameManagerEx gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManagerEx>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D whatDidIHit)
+    {
+        if(whatDidIHit.tag == "Player")
+        {
+            whatDidIHit.GetComponent<PlayerControllerEx>().LoseALife();
+            Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+        } else if(whatDidIHit.tag == "Weapons")
+        {
+            Destroy(whatDidIHit.gameObject);
+            Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+            gameManager.AddScore(5);
+            Destroy(this.gameObject);
+        }
+    }
+}
